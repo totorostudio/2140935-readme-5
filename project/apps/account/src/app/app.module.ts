3d6cdf37b0a8c3ad5a91cbd576938468e840adc2
Andrew Stepanov <stepanov.management@gmail.com>
2023-12-08 09:48:57 +0500

    Создал заготовки AuthenticationModule и BlogUserModule

--- project/apps/account/src/app/app.module.ts
+++ project/apps/account/src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { BlogUserModule } from './blog-user/blog-user.module';
+import { AuthenticationModule } from './authentication/authentication.module';
 
 @Module({
-  imports: [],
+  imports: [BlogUserModule, AuthenticationModule],
   controllers: [],
   providers: [],
 })
