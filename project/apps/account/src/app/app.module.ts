2b9d6efdbf75a524a324bdf6f2ef7996483f9134
Andrew Stepanov <stepanov.management@gmail.com>
2023-12-15 23:41:24 +0500

    Завершил настройку подключения к Mongo, подключается

--- project/apps/account/src/app/app.module.ts
+++ project/apps/account/src/app/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { BlogUserModule } from './blog-user/blog-user.module';
 import { AuthenticationModule } from './authentication/authentication.module';
-import { ConfigAccountModule } from '@project/shared/config/account';
+import { ConfigAccountModule, getMongooseOptions } from '@project/shared/config/account';
 
 @Module({
-  imports: [BlogUserModule, AuthenticationModule, ConfigAccountModule],
+  imports: [
+    BlogUserModule,
+    AuthenticationModule,
+    ConfigAccountModule,
+    MongooseModule.forRootAsync(
+      getMongooseOptions()
+    )
+  ],
   controllers: [],
   providers: [],
 })
--- project/libs/shared/config/account/src/index.ts
+++ project/libs/shared/config/account/src/index.ts
@@ -1,1 +1,2 @@
 export * from './lib/config-account.module';
+export * from './lib/mongodb/get-mongoose-options';
