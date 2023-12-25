f511ea6d234f7839d4215d4583a2a8fbfdbfa357
Andrew Stepanov <stepanov.management@gmail.com>
2023-12-25 18:14:19 +0500

    Описал интерфейсы для всех типов постов

--- project/libs/shared/app/types/src/lib/post.interface.ts
+++ project/libs/shared/app/types/src/lib/post.interface.ts
@@ -1,13 +1,52 @@
 import { Tag } from './tag.interface';
+import { Comment } from './comment.interface';
+
+export const PostType = {
+  TEXT: 'text',
+  VIDEO: 'video',
+  QUOTE: 'quote',
+  PHOTO: 'photo',
+  LINK: 'link',
+} as const;
+
+export type PostType = typeof PostType[keyof typeof PostType];
 
 export interface BasePost {
   id?: string;
+  originalId?: string;
+  isRepost?: boolean;
+  isDraft?: boolean;
+  type: PostType;
   title: string;
+  likesCount?: number;
+  commentsCount?: number;
   tags: Tag[];
-  description: string;
-  content: string;
+  comments: Comment[];
   createdAt?: Date;
   updatedAt?: Date;
+  originalAuthor?: string;
   userId: string;
-  comments: Comment[];
+}
+
+export interface TextPost extends BasePost {
+  description: string;
+  content: string;
+}
+
+export interface VideoPost extends BasePost {
+  videoUrl: string;
+}
+
+export interface QuotePost extends BasePost {
+  quote: string;
+  quoteAuthor: string;
+}
+
+export interface PhotoPost extends BasePost {
+  photoUrl: string;
+}
+
+export interface LinkPost extends BasePost {
+  description: string;
+  linkUrl: string;
 }
