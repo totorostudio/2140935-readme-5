4379310db01ccf32903ac6921302dfb91121bfa5
Andrew Stepanov <stepanov.management@gmail.com>
2024-01-12 11:05:27 +0500

    Куча всего про blog-tag

---
+++ project/apps/blog/src/app/blog-tag/blog-tag.controller.ts
@@ -0,0 +1,43 @@
+import { Controller, Get, Param, Post, Body, Delete, Patch, HttpCode, HttpStatus } from '@nestjs/common';
+import { BlogTagService } from './blog-tag.service';
+import { CreateTagDto } from './dto/create-tag.dto';
+import { fillDto } from '@project/shared/helpers';
+import { TagRdo } from './rdo/tag.rdo';
+import { UpdateTagDto } from './dto/update-tag.dto';
+
+@Controller('tags')
+export class BlogTagController {
+  constructor(
+    private readonly blogTagService: BlogTagService
+  ) {}
+
+  @Get('/:id')
+  public async show(@Param('id') id: string) {
+    return this.blogTagService.getTag(id);
+  }
+
+  @Get('/')
+  public async index() {
+    const blogTagEntities = await this.blogTagService.getAllCategories();
+    const categories = blogTagEntities.map((blogTag) => blogTag.toPOJO());
+    return fillDto(TagRdo, categories);
+  }
+
+  @Post('/')
+  public async create(@Body() dto: CreateTagDto) {
+    const newTag = await this.blogTagService.createTag(dto);
+    return fillDto(TagRdo, newTag.toPOJO());
+  }
+
+  @Delete('/:id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  public async destroy(@Param('id') id: string) {
+    await this.blogTagService.deleteTag(id);
+  }
+
+  @Patch('/:id')
+  public async update(@Param('id') id: string, @Body() dto: UpdateTagDto) {
+    const updatedTag = await this.blogTagService.updateTag(id, dto);
+    return fillDto(TagRdo, updatedTag.toPOJO());
+  }
+}
