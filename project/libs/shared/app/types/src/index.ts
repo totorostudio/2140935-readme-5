3d28a8604ae2e6f11f59d7a075425c01e2ed58b8
Andrew Stepanov <stepanov.management@gmail.com>
2024-01-17 08:51:20 +0500

    Добавил типы для сортировки и пагинации

--- project/libs/shared/app/types/src/index.ts
+++ project/libs/shared/app/types/src/index.ts
@@ -4,3 +4,5 @@ export * from './lib/auth-user.interface';
 export * from './lib/tag.interface';
 export * from './lib/comment.interface';
 export * from './lib/post.interface';
+export * from './lib/sort-direction.interface';
+export * from './lib/pagination.interface';
