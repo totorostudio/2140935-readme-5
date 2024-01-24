45c7df079052cfee306cdcdb48f940bc5b0466d6
Andrew Stepanov <stepanov.management@gmail.com>
2024-01-24 11:12:22 +0500

    Добавил рассылку email

--- project/apps/notify/src/app/email-subsriber/email-subscriber.controller.ts
+++ project/apps/notify/src/app/email-subsriber/email-subscriber.controller.ts
@@ -4,11 +4,13 @@ import { RabbitSubscribe } from '@golevelup/nestjs-rabbitmq';
 import { EmailSubscriberService } from './email-subscriber.service';
 import { CreateSubscriberDto } from './dto/create-subscriber.dto';
 import { RabbitRouting } from '@project/libs/shared/app/types';
+import { MailService } from '../mail/mail.service';
 
 @Controller()
 export class EmailSubscriberController {
   constructor(
     private readonly subscriberService: EmailSubscriberService,
+    private readonly mailService: MailService,
   ) {}
 
   @RabbitSubscribe({
@@ -18,5 +20,6 @@ export class EmailSubscriberController {
   })
   public async create(subscriber: CreateSubscriberDto) {
     this.subscriberService.addSubscriber(subscriber);
+    this.mailService.sendNotifyNewSubscriber(subscriber);
   }
 }
--- project/apps/notify/src/app/email-subsriber/email-subscriber.module.ts
+++ project/apps/notify/src/app/email-subsriber/email-subscriber.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { RabbitMQModule } from '@golevelup/nestjs-rabbitmq';
-
 import { EmailSubscriberModel, EmailSubscriberSchema } from './email-subscriber.model';
 import { EmailSubscriberService } from './email-subscriber.service';
 import { EmailSubscriberRepository } from './email-subscriber.repository';
 import { getRabbitMQOptions } from '@project/shared/helpers';
 import { EmailSubscriberController } from './email-subscriber.controller';
+import { MailModule } from '../mail/mail.module';
 
 @Module({
   imports: [
@@ -17,6 +17,7 @@ import { EmailSubscriberController } from './email-subscriber.controller';
       RabbitMQModule,
       getRabbitMQOptions('application.rabbit')
     ),
+    MailModule,
   ],
   controllers: [
     EmailSubscriberController,
