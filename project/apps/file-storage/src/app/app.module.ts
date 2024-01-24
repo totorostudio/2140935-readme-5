947d66c3a0749f92e66a8a0e15e19b065bb46c0f
Andrew Stepanov <stepanov.management@gmail.com>
2024-01-24 09:51:55 +0500

    Настроил докер для Mongo и настройки подключения к БД

--- project/apps/file-storage/src/app/app.module.ts
+++ project/apps/file-storage/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { FileUploaderModule } from './file-uploader/file-uploader.module';
-import { FileStorageConfigModule } from '@project/shared/config/file-storage';
+import { FileStorageConfigModule, getMongooseOptions } from '@project/shared/config/file-storage';
 
 @Module({
   imports: [
     FileUploaderModule,
     FileStorageConfigModule,
+    MongooseModule.forRootAsync(getMongooseOptions()),
   ],
   controllers: [],
   providers: [],
--- project/libs/shared/config/file-storage/src/index.ts
+++ project/libs/shared/config/file-storage/src/index.ts
@@ -1,2 +1,3 @@
 export * from './lib/file-storage-config.module';
 export { default as FileStorageConfig } from './lib/file-storage.config';
+export * from './lib/file-storage.mongoose-options';
