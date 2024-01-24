61498888b393b9b35fb92903f038e1fd637b5a76
Andrew Stepanov <stepanov.management@gmail.com>
2024-01-24 07:55:08 +0500

    Подключил и настроил JWT

--- project/libs/shared/app/types/src/index.ts
+++ project/libs/shared/app/types/src/index.ts
@@ -6,3 +6,5 @@ export * from './lib/comment.interface';
 export * from './lib/post.interface';
 export * from './lib/sort-direction.interface';
 export * from './lib/pagination.interface';
+export * from './lib/token-payload.interface';
+export * from './lib/token.interface';
