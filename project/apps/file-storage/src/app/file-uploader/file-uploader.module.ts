b801d26a7eb43c6d8b9e66121a7e760e4b9dfc1a
Andrew Stepanov <stepanov.management@gmail.com>
2024-01-24 09:32:50 +0500

    Настроил имена файлов и папок для файлов

--- project/apps/file-storage/src/app/file-uploader/file-uploader.module.ts
+++ project/apps/file-storage/src/app/file-uploader/file-uploader.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { ConfigService } from '@nestjs/config';
 
 import { FileUploaderService } from './file-uploader.service';
 import { FileUploaderController } from './file-uploader.controller';
 
+const SERVE_ROOT = '/static';
+
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const rootPath = configService.get<string>('application.uploadDirectory');
+        return [{
+          rootPath,
+          serveRoot: SERVE_ROOT,
+          serveStaticOptions: {
+            fallthrough: true,
+            etag: true,
+          }
+        }]
+      }
+    })
+  ],
   providers: [FileUploaderService],
   controllers: [FileUploaderController],
 })
--- project/apps/file-storage/src/app/file-uploader/file-uploader.service.ts
+++ project/apps/file-storage/src/app/file-uploader/file-uploader.service.ts
@@ -4,6 +4,9 @@ import { ConfigType } from '@nestjs/config';
 import { ensureDir } from 'fs-extra';
 import { writeFile } from 'node:fs/promises';
 import { join } from 'node:path';
+import dayjs from 'dayjs';
+import { randomUUID } from 'node:crypto';
+import { extension } from 'mime-types';
 
 import { FileStorageConfig } from '@project/shared/config/file-storage';
 
@@ -17,17 +20,21 @@ export class FileUploaderService {
   ) {}
 
   private getUploadDirectoryPath(): string {
-    return this.config.uploadDirectory;
+    const [year, month] = dayjs().format('YYYY MM').split(' ');
+    return join(this.config.uploadDirectory, year, month);
   }
 
   private getDestinationFilePath(filename: string): string {
-    return join(this.getUploadDirectoryPath(), filename)
+    return join(this.getUploadDirectoryPath(), filename);
   }
 
   public async saveFile(file: Express.Multer.File): Promise<string> {
     try {
       const uploadDirectoryPath = this.getUploadDirectoryPath();
-      const destinationFile = this.getDestinationFilePath(file.originalname);
+      const filename = randomUUID();
+      const fileExtension = extension(file.mimetype);
+
+      const destinationFile = this.getDestinationFilePath(`${filename}.${fileExtension}`);
 
       await ensureDir(uploadDirectoryPath);
       await writeFile(destinationFile, file.buffer);
