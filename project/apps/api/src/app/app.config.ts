46d5d9057af6c11aca68a543ab50f107de0439a4
Andrew Stepanov <stepanov.management@gmail.com>
2024-01-26 10:21:28 +0500

    Сделал структуру для сервиса API

---
+++ project/apps/api/src/app/app.config.ts
@@ -0,0 +1,6 @@
+export enum ApplicationServiceURL {
+  Users = 'http://localhost:3333/api/auth',
+}
+
+export const HTTP_CLIENT_MAX_REDIRECTS = 5;
+export const HTTP_CLIENT_TIMEOUT = 5000;
--- project/apps/api/src/app/app.module.ts
+++ project/apps/api/src/app/app.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
-
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UsersController } from './users.controller';
+import { BlogController } from './blog.controller';
+import { HttpModule } from '@nestjs/axios';
+import { HTTP_CLIENT_MAX_REDIRECTS, HTTP_CLIENT_TIMEOUT } from './app.config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    HttpModule.register({
+      timeout: HTTP_CLIENT_TIMEOUT,
+      maxRedirects: HTTP_CLIENT_MAX_REDIRECTS,
+    })
+  ],
+  controllers: [
+    UsersController,
+    BlogController,
+  ],
+  providers: [],
 })
 export class AppModule {}
